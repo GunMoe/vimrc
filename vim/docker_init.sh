643fc512e7423e7209fad3681469efb702efbcf7
GunMoe <i@dounait.cn>
2020-07-24 23:50:53 +0800

    add docker_init.sh

--- vim/docker_init.sh
+++ vim/docker_init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt update
+apt install vim git 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp $PWD/config/vimrc ~/.vimrc
 vim +PluginInstall +qall
